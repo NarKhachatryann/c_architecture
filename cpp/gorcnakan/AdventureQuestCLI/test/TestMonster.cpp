867a78b44a1ade77ebdadf6b6afc0d842c57e67b
NaroArmenia <naroarmenia9@gmail.com>
2025-03-24 20:27:39 +0400

    Game

--- cpp/gorcnakan/AdventureQuestCLI/test/TestMonster.cpp
+++ cpp/gorcnakan/AdventureQuestCLI/test/TestMonster.cpp
@@ -1,15 +1,13 @@
 #include "../include/Monster.h"
 #include <gtest/gtest.h>
 
-// Թեստավորում ենք կոնստրուկտորի ճիշտ աշխատանքի համար
 TEST(MonsterTest, ConstructorInitialization) {
     Monster monster("TestMonster", 80, 15, 5, "Poison Strike");
 
     EXPECT_EQ(monster.getHealth(), 80);
-    EXPECT_NO_THROW(monster.displayStats()); // Ստուգում ենք, որ displayStats() չի կրակում բացառում
+    EXPECT_NO_THROW(monster.displayStats());
 }
 
-// Թեստավորում ենք takeDamage() ֆունկցիոնալությունը
 TEST(MonsterTest, TakeDamageFunctionality) {
     Monster monster("TestMonster", 80, 15, 5, "Poison Strike");
 
@@ -17,12 +15,11 @@ TEST(MonsterTest, TakeDamageFunctionality) {
     EXPECT_EQ(monster.getHealth(), 50);
 
     monster.takeDamage(100);
-    EXPECT_EQ(monster.getHealth(), 0); // Վստահվում ենք, որ առողջությունը հասնում է 0
+    EXPECT_EQ(monster.getHealth(), 0);
 }
 
-// Թեստավորում ենք հատուկ հմտության ցուցադրումը
 TEST(MonsterTest, SpecialAbilityDisplay) {
     Monster monster("TestMonster", 80, 15, 5, "Poison Strike");
 
-    EXPECT_NO_THROW(monster.displayStats()); // Վստահվում ենք, որ հատուկ կարողությունը ճիշտ է ցուցադրվում
+    EXPECT_NO_THROW(monster.displayStats());
 }
