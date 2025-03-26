76ef0f34a6218e8cd1a96ddb87905909c380c204
NaroArmenia <naroarmenia9@gmail.com>
2025-03-26 19:57:59 +0400

    issue 26

--- cpp/gorcnakan/AdventureQuestCLI/src/main.cpp
+++ cpp/gorcnakan/AdventureQuestCLI/src/main.cpp
@@ -1,6 +1,23 @@
-#include <iostream>
+#include "../include/Character.h"
+#include "../include/Combatable.h"
+#include "../include/GameWorld.h"
+#include "../include/Hero.h"
+#include "../include/Interactable.h"
+#include "../include/Monster.h"
+#include "../include/NPC.h"
+#include "../include/QuestManager.h"
+#include "../include/Utility.h"
 
 int main() {
-    std::cout << "Բարի գալուստ Adventure Quest խաղին!" << std::endl;
-    return 0; // Մուտքի կետ՝ ծրագրի համար
+    GameWorld game;
+    Hero hero("Armen", 100, 15, 10, 120);
+    Monster dragon("Dragon", 150, 20, 5, 150, "Fire Breath");
+
+    game.addEntity(&hero);
+    game.addEntity(&dragon);
+
+    hero.displayStats();
+    dragon.displayStats();
+
+    return 0;
 }
